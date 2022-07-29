b9a8d6992203e284f0396ea3d8fd0ecfaa2ddb23
zoomo <phengaroune.chan@gmail.com>
2022-07-29 13:13:36 -0400

    update

--- js/putRequest.js
+++ js/putRequest.js
@@ -1,15 +1,15 @@
 export function putRequest(url, json) {
-  	console.log("SENDING PUT REQUEST TO: " + url);
+	console.log("SENDING PUT REQUEST TO: " + url);
 
-  	// Opening a PUT request to the URL
-  	let request = new XMLHttpRequest();
-  	request.open("PUT", url, true);
+	// Opening a PUT request to the URL
+	let request = new XMLHttpRequest();
+	request.open("PUT", url, true);
 
 	// Setting the header of the API request
-    request.setRequestHeader("Content-type", "application/json");
+	request.setRequestHeader("Content-type", "application/json");
 
-  	// Asynchronously set up request
-  	request.onload = function () {
+	// Asynchronously set up request
+	request.onload = function () {
 		console.log("PUT Request is sent!");
 
 		// if the request is good and valid, return the response
@@ -17,20 +17,20 @@ export function putRequest(url, json) {
 			console.log("Status Text: " + request.statusText);
 			console.log("Response: " + request.response);
 			return request.response;
-		} 
+		}
 
-        // if request is a redirect, return an unexpected error
-        else if (request.status >= 300 && request.status < 400) {
+		// if request is a redirect, return an unexpected error
+		else if (request.status >= 300 && request.status < 400) {
 			console.log(request.status);
 			alert("UNEXPECTED REDIRECT RESPONSE! " + request.status);
-            return null;
-        } 
-        
-        //Error handling
-        else {
+			return null;
+		}
+
+		//Error handling
+		else {
 			console.log(request.status);
-			alert("PUT REQUEST FAILED! " + request.status );
-            return null;
+			alert("PUT REQUEST FAILED! " + request.status);
+			return null;
 		}
 	};
 
---
+++ js/wishlist.js
@@ -0,0 +1,1 @@
+console.log("Loaded wishlist.js");
