1ee7536a6c38b1bc70903b35716635902d601eb4
zoomo <phengaroune.chan@gmail.com>
2022-08-01 08:42:31 -0400

    update

--- js/wishlist.js
+++ js/wishlist.js
@@ -1,4 +1,5 @@
-let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+// let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+let url = "http://pokepost-env.eba-jzp4ndpr.us-east-1.elasticbeanstalk.com/pokemon/" + pokemon_name;
 
 document.getElementById('getData').onclick = getData;
 
@@ -6,7 +7,7 @@ function getData() {
     let userInput = document.getElementById('dataInput').value;
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = receiveData;
-    xhttp.open('GET', apiURL + '' + userInput);
+    xhttp.open('GET', url + '' + userInput);
     xhttp.send();
 
     function receiveData() {
@@ -25,31 +26,54 @@ function getData() {
             }
         }
     }
+}
 
-    function populateData(response) {
-        let dataSection = document.getElementById('data');
+function populateData(response) {
+    let dataSection = document.getElementById('data');
 
-        let nameTag = document.createElement('h3');
-        nameTag.innerHTML = capitalize(response.name);
-        let abilitiesArray = response.abilities;
-        let abilities = document.createElement('ul')
-        for (let ability of abilitiesArray) {
-            let abilityLi = document.createElement('li');
-            abilityLi.innerHTML = capitalize(ability.ability.name);
-            abilities.appendChild(abilityLi);
-        }
+    let nameTag = document.createElement('h3');
+    nameTag.innerHTML = capitalize(response.name);
+    let abilitiesArray = response.abilities;
+    let abilities = document.createElement('ul')
+    for (let ability of abilitiesArray) {
+        let abilityLi = document.createElement('li');
+        abilityLi.innerHTML = capitalize(ability.ability.name);
+        abilities.appendChild(abilityLi);
+    }
 
-        dataSection.appendChild(nameTag);
-        dataSection.innerHTML += 'Abilities<br>';
-        dataSection.appendChild(abilities);
+    dataSection.appendChild(nameTag);
+    dataSection.innerHTML += 'Abilities<br>';
+    dataSection.appendChild(abilities);
 
-        let imageUrl = response.sprites;
-        for (let sprite in imageUrl) {
-            if (imageUrl[sprite]) {
-                let spriteImg = document.createElement('img');
-                spriteImg.src = imageUrl[sprite];
-                dataSection.appendChild(spriteImg);
-            }
+    let spirtesObject = response.sprites;
+    for (let sprite in spirtesObject) {
+        if (spirtesObject[sprite]) {
+            let spriteImg = document.createElement('img');
+            spriteImg.src = spirtesObject[sprite];
+            dataSection.appendChild(spriteImg);
         }
     }
+}
+
+function capitalize(str) {
+    if (str)
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    else
+        return '';
+}
+
+for (let i = 0; i < 10; i++) {
+const img = document.createElement("img");
+img.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png";
+document.body.appendChild(img);
+}
+
+function displayResult() {
+    var firstRow=document.getElementById('listPokemon').rows[0];
+    var x=firstRow.insertCell(-1);
+    x.innerHTML="Pokemon Name";
+
+    var img = document.createElement('img');
+    img.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png";
+    x.appendChild(img);
 }
